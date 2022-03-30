142019af8c87c47bb9620dc50a52ce191f4a6b70
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-30 23:30:07 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import {data} from './components/data'
 import './App.css';
 import Header from './components/Header/Header';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      merhaba
-    <Header/>
+      <Router>
+      <Header/>
+
+      </Router>
+    
 
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
+
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div>
+        <h1>
+          <Link to="/" className="Logo">
+            Kleidung
+          </Link>
+        </h1>
+      </div>
+      <div className="header-links">
+          <ul>
+              <li>
+                  <Link to='/'>Home</Link>
+              </li>
+          </ul>
+          <ul>
+              <li>
+                  <Link to='/Signup'>Signup</Link>
+              </li>
+          </ul>
+          <ul>
+              <li>
+                  <Link to='/Cart' className="cart">
+                  <i class="fas fa-shopping-cart"></i>
 
-export default Header
+                  </Link>
+              </li>
+          </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
