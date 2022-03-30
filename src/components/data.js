e894907e09b6fd70355f07aefa7443f72b2d424d
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-30 22:44:36 +0200

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,50 @@
+const data ={
+    productItem:[
+        {
+            id:1,
+            name:'aaaaaaa',
+            price:456,
+            image:'./img/1.jpg'
+        },
+        {
+            id:2,
+            name:'auuuua',
+            price:156,
+            image:'./img/2.jpg'
+        },
+        {
+            id:3,
+            name:'yyyyyaaa',
+            price:956,
+            image:'./img/3.jpg'
+        },
+        {
+            id:4,
+            name:'aeeeea',
+            price:5456,
+            image:'./img/4.jpg'
+        },
+        {
+            id:5,
+            name:'addda',
+            price:4356,
+            image:'./img/5.jpg'
+        },
+        {
+            id:6,
+            name:'abcccca',
+            price:4156,
+            image:'./img/6.jpg'
+        },
+        {
+            id:7,
+            name:'abbb',
+            price:4536,
+            image:'./img/7.jpg'
+        }
+
+
+
+    ]
+}
+export default data;
